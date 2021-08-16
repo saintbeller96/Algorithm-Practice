222e20458135ba9038f4467ddff8fc9259854d24
beller <rlawhdtjd9@naver.com>
2021-08-16 23:48:51 +0900

    Solved: Boj 13707

--- Boj/src/P13707합분해2_DP/Main.java
+++ Boj/src/P13707합분해2_DP/Main.java
@@ -9,12 +9,15 @@ public class Main {
 		StringTokenizer stk = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(stk.nextToken());
 		int K = Integer.parseInt(stk.nextToken());
-		long MOD = 1000000000;
-		long[][] dp = new long[N+1][K+1];
-		for (int n = 1; n <=N ; n++) {
-			for (int k = 1; k <=K ; k++) {
-				if(n == 1) dp[n][k] = k;
-				else dp[n][k] = (dp[n][k-1] + dp[n-1][k])%MOD;
+		int MOD = 1000000000;
+		int[][] dp = new int[5001][5001];
+		for (int n = 1; n <=5000 ; n++) {
+			dp[n][1] = 1;
+			dp[1][n] = n;
+		}
+		for (int n = 2; n <=N ; n++) {
+			for (int k = 2; k <=K ; k++) {
+				dp[n][k] = (dp[n][k-1] + dp[n-1][k])%MOD;
 			}
 		}
 		System.out.println(dp[N][K]);
